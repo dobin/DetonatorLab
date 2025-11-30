fc1c541e2dc3f6be01cdb8e56a804c4166364451
Dobin Rutishauser <dobin.rutishauser@gmail.com>
2025-11-30 16:20:42 +0100

    fix

--- loader/loader_5a_dll/loader_5a_dll.c
+++ loader/loader_5a_dll/loader_5a_dll.c
@@ -60,7 +60,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 __declspec(dllexport) int process(void)
 {
 	DWORD result;
-	char xorkey[] = "{}";
+	{{SHELLCODE}}
 
     antiemulation();
 
